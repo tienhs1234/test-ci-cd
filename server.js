b56b0aa3e05c49d8d5f2d5a2864ed0b8e0ba81c2
TienTien <tienhs1234@gmail.com>
2024-02-25 00:22:59 +0700

    add Project

---
+++ server.js
@@ -0,0 +1,16 @@
+const favoriteFood = process.env.NAME;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while(true) {
+    console.log(`My favorite food is ${favoriteFood}`);
+    console.log("Microservices rock!");
+    await sleep(5000);
+  }
+  
+}
+
+main();
